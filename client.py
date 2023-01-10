2fcc519c2accb8325e673b1286240e5401cbdfdb
Mansha03 <manshasharma763@gmail.com>
2023-01-10 11:49:29 +0530

    updated

--- client.py
+++ client.py
@@ -58,5 +58,4 @@ if __name__ == "__main__":
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
             prices[stock]=price
-
         print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
